2368f935c3e227430a6db62b49cd9ec93f698104
xatixatix <xatixatix@gmail.com>
2021-03-19 18:09:44 +0100

    drill 15.2 elso ket feladat

--- week02-06/Drill15/drill15.2.cpp
+++ week02-06/Drill15/drill15.2.cpp
@@ -1,15 +1,29 @@
-#include <iostream>
+#include "std_lib_facilities.h"
 
 /*
-g++ drill15.2.cpp -o drill15`
+g++ drill15.2.cpp -o drill15.2
 */
 
-class Person{
-	
-}
+struct Person{
+public:
+	Person(string nameIn, int ageIn):firstName{nameIn}, age{ageIn}{};
+	Person(string firstNameIn, string secondNameIn, int ageIn):firstName{firstNameIn}, secondName{secondNameIn}, age{ageIn}{};
+
+	string getFirstName() const {return firstName;}
+	string getSecondName() const {return secondName;}
+	int getAge() const {return age;}
+
+private:
+	string firstName;
+	string secondName;
+	int age;
+};
 
 int main()
 try{
+
+Person gof{"Goofy", 63};
+cout << gof.getFirstName() << " is " << gof.getAge() << " years old." << endl;
 
 }
 catch(exception&e){
