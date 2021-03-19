7d284de875fc953f29f759a07a198b0e8e63de12
xatixatix <xatixatix@gmail.com>
2021-03-19 16:28:17 +0100

    drill14 kesz

--- week02-06/Drill14/drill14.cpp
+++ week02-06/Drill14/drill14.cpp
@@ -5,7 +5,7 @@ using namespace std;
 g++ drill14.cpp drill14`
 */
 
-class B1{
+class B1:{
 public:
 	virtual void vf() const {
 		cout<<"B1::vf()\n";
@@ -15,10 +15,11 @@ public:
 		cout<<"B1::f()\n";
 	}
 
-	//virtual void pvf() const = 0;
+	virtual void pvf() const = 0;
 };
 
 class D1:public B1{
+public:
 	void vf() const override { 
 		cout<<"D1::vf()\n";
 	}
@@ -29,10 +30,36 @@ class D1:public B1{
 };
 
 class D2:public D1{
-	// void pvf() const override { 
-	// 	cout<<"D2::pvf()\n";
-	// }
+public:
+	void pvf() const override { 
+		cout<<"D2::pvf()\n";
+	}
 };
+
+class B2{
+public:
+	virtual void pvf() const = 0;
+};
+
+class D21:public B2{
+public:
+	string valami = "valami";
+	void pvf() const override { 
+		cout<<valami<<endl;
+	}
+};
+
+class D22:public B2{
+public:
+	int valami = 5;
+	void pvf() const override { 
+		cout<<valami<<endl;
+	}
+};
+
+void f(B2& b2){
+	b2.pvf();
+}
 
 void call(B1& b){
 	b.vf();
@@ -42,6 +69,7 @@ void call(B1& b){
 int main()
 try{
 
+/*
 //1
 B1 b1obj;
 call(b1obj);
@@ -53,14 +81,19 @@ call(d1obj);
 //3
 B1& b1ref {d1obj};
 call(b1ref);
+*/
 
-/*
 //6
 D2 d2obj;
 d2obj.f();
 d2obj.vf();
 d2obj.pvf();
-*/
+
+D21 d21obj;
+D22 d22obj;
+
+f(d21obj);
+f(d22obj);
 
 }
 catch(exception&e){
--- week02-06/Drill15/drill15.cpp
+++ week02-06/Drill15/drill15.cpp
@@ -5,10 +5,16 @@
 g++ drill15.cpp Graph.cpp Window.cpp GUI.cpp Simple_window.cpp -o drill15 `fltk-config --ldflags --use-images`
 */
 
+double one(double) {return 1;}
+double slope(double x) {return x/2;}
+double square(double x) {return x*x;}
+
 int main()
 try{
 
 using namespace Graph_lib;
+
+
 
 constexpr int h = 1000;
 constexpr int w = 600;
@@ -18,9 +24,25 @@ constexpr int middley = h/2;
 
 Point orig{middley, middlex};
 
+constexpr int r_min = -20;
+constexpr int r_max = 11;
+
+constexpr int n_points = 400;
+constexpr int x_scale = 30;
+constexpr int y_scale = 30;
+
 Simple_window win{Point{50,50}, w, h, "Function graphs"};
 win.wait_for_button();
 
+Function s{one,r_min,r_max,orig,n_points,x_scale,y_scale};
+Function s2{slope,r_min,r_max,orig,n_points,x_scale,y_scale};
+Function s3{square,r_min,r_max,orig,n_points,x_scale,y_scale};
+
+win.attach(s);
+win.attach(s2);
+win.attach(s3);
+
+win.wait_for_button();
 }
 catch(exception&e){
 	cerr<< "exception" << e.what() << '\n';
